16da7eedddc092aa3c35364b317947a2e6878b7e
Andrei Popa <andrei@hashicorp.com>
2020-03-10 18:18:18 +0100

    add more output

--- example/output.tf
+++ example/output.tf
@@ -1,3 +1,18 @@
 output "rds_ip" {
   value = module.rds.rds_ip
 }
+
+output "db_name" {
+  description = "DB name"
+  value = module.rds.db_name
+}
+
+output "db_port" {
+  description = "DB port"
+  value = module.rds.db_port
+}
+
+output "db_username" {
+  description = "DB username"
+  value = module.rds.db_username
+}
